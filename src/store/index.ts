1f654e26449395663d609a4d1e1fd870e0a42f1f
Aliakbar <aliakbar.ghasemi1371@gmail.com>
2023-01-25 20:56:51 +0330

    init vuex basic ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,34 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import { RootState } from "../types/state/IRootState";
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+import createPersistedState from "vuex-persistedstate";
+import * as Cookies from "js-cookie";
 
-export default new Vuex.Store({
+import { user } from "./modules/user";
+import { discussion } from "./modules/discussion";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store<RootState>({
+  plugins: [
+    createPersistedState({
+      storage: {
+        getItem: (key) => Cookies.get(key),
+        // Please see https://github.com/js-cookie/js-cookie#json, on how to handle JSON.
+        setItem: (key, value) =>
+          Cookies.set(key, value, { expires: 3, secure: true }),
+        removeItem: (key) => Cookies.remove(key),
+      },
+    }),
+  ],
   state: {
+    version: "1.0.0",
   },
-  mutations: {
-  },
-  actions: {
-  },
+  mutations: {},
+  actions: {},
   modules: {
-  }
-})
+    user,
+    discussion,
+  },
+});
