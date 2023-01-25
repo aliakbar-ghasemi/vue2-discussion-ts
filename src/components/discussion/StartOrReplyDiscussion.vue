7d19dc624fe7cfd7ca687e06ba611e8c486cabd6
Aliakbar <aliakbar.ghasemi1371@gmail.com>
2023-01-25 21:37:27 +0330

    start or reply discussion

--- src/components/discussion/StartOrReplyDiscussion.vue
+++ src/components/discussion/StartOrReplyDiscussion.vue
@@ -1,10 +1,70 @@
 <template>
-  <div></div>
+  <div class="d-flex">
+    <rounded-image :src="user.avatar" :title="user.name" />
+    <v-textarea
+      v-model="discussionText"
+      rounded-xl
+      :label="isReply ? 'Reply' : 'Start a discussion'"
+      flat
+      class="ml-3 my-auto"
+      solo
+      outlined
+      dense
+      hide-details=""
+      auto-grow
+      rows="1"
+      @keydown.enter.prevent="submit"
+    ></v-textarea>
+  </div>
 </template>
 
-<script>
-export default {};
-</script>
+<script lang="ts">
+import RoundedImage from "../widget/RoundedImage.vue";
+import { mapGetters } from "vuex";
 
-<style lang="scss" scoped>
-</style>
+export default {
+  components: { RoundedImage },
+  props: {
+    isReply: {
+      type: Boolean,
+      default: false,
+    },
+    discussionId: {
+      type: String,
+      default: null,
+    },
+  },
+
+  computed: {
+    ...mapGetters('user', ['user'])
+  },
+
+  data() {
+    return {
+      discussionText: "",
+    };
+  },
+  methods: {
+    submit(): void {
+      if (!this.discussionText) {
+        return;
+      }
+
+      if (this.isReply) {
+        //Reply discussion
+        this.$store.dispatch("discussion/replyDiscussion", {
+          discussionText: this.discussionText,
+          discussionId: this.discussionId,
+        });
+      } else {
+        //Start discussion
+        this.$store.dispatch("discussion/startDiscussion", {
+          discussionText: this.discussionText,
+        });
+      }
+
+      this.discussionText = ""
+    },
+  },
+};
+</script>
--- src/store/modules/discussion/actions.ts
+++ src/store/modules/discussion/actions.ts
@@ -1,5 +1,34 @@
+import { Discussion } from '@/types/IDiscussion';
 import { RootState } from "./../../../types/state/IRootState";
 import { DiscussionState } from "@/types/state/IDiscussionState";
 import { ActionTree } from "vuex";
 
-export const actions: ActionTree<DiscussionState, RootState> = {};
+export const actions: ActionTree<DiscussionState, RootState> = {
+    startDiscussion(context, { discussionText }) {        
+        const discussion: Discussion = {
+          id: new Date().getTime(),
+          date: new Date().getTime(),
+          user: context.rootGetters['user/user'],
+          text: discussionText,
+          likes: 0,
+          iLikedIt: false,
+          replies: [],
+        };
+    
+        context.commit("addDiscussion", discussion);
+      },
+
+      replyDiscussion(context, { discussionText, discussionId }) {
+        const discussion = {
+          id: new Date().getTime(),
+          date: new Date().getTime(),
+          user: context.rootGetters['user/user'],
+          text: discussionText,
+          likes: 0,
+          iLikedIt: false,
+          discussionForeignKey: discussionId,
+        };
+    
+        context.commit("addDiscussion", discussion);
+      },
+};
