64381dfe353a466e55f8869f550dc39c51647045
Aliakbar <aliakbar.ghasemi1371@gmail.com>
2023-01-25 21:09:11 +0330

    create discussions component

--- src/components/discussion/Discussions.vue
+++ src/components/discussion/Discussions.vue
@@ -1,10 +1,27 @@
 <template>
-  <div></div>
+  <div>
+    <div v-for="discussion in discussions" :key="discussion.id">
+      <hr />
+      <Discussion :discussion="discussion" class="my-6 px-8" />
+    </div>
+  </div>
 </template>
 
-<script>
-export default {};
+<script lang="ts">
+import Discussion from "./Discussion.vue";
+import { mapGetters } from "vuex";
+export default {
+  components: {
+    Discussion,
+  },
+  computed: {
+    ...mapGetters('discussion', ['discussions'])
+  },
+};
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+hr {
+  border: solid 0.1rem #ebeff3;
+}
 </style>
